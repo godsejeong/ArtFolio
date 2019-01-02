a641149d1adcac16661b62bf05b0e74c7181f8bc
나재민 <jaemin@najaemin-ui-MacBook-Pro.local>
2019-01-02 05:35:38 -0300

    [add] add webview bridge & webview setting

--- app/src/main/java/com/didimstory/artfolioapplication/activity/CameraActivity.kt
+++ app/src/main/java/com/didimstory/artfolioapplication/activity/CameraActivity.kt
@@ -1,12 +1,16 @@
-package com.didimstory.artfolioapplication
+package com.didimstory.artfolioapplication.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.didimstory.artfolioapplication.R
 
 class CameraActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+
+
     }
 }
